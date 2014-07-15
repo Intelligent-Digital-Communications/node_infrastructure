aad4b7c942d8372a6bc2e05a3f94fb6d03d32dbd
Paul Garver <pwg@lawn-143-215-53-120.lawn.gatech.edu>
2014-07-15 11:27:03 -0400

    Adding a script to show how to monitor the ADC on the ethernet relay

---
+++ relay_cmds/temp_sensor_script.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Reads an 8-bit ADC from a National Control Devices (controlanything.com) Ethernet Relay
+# PWG 7.15.2014
+a=0
+RELAY_IP=192.168.104.104
+RELAY_PORT=2101
+RELAY_CMD="relay_readadc1_8bit.hex"
+# Update Rate (sec)
+UPDATE_TIME=10
+while [ "$a" == 0 ]; do
+ val=`cat $RELAY_CMD | nc $RELAY_IP $RELAY_PORT | hexdump -v -e '"%d"'` 
+ # Convert 8-bit temperature value
+ temp=$(echo "scale=2; ${val}*5*100/255" | bc -l) 
+ echo "`date`|$temp deg C"
+ sleep $UPDATE_TIME
+done
