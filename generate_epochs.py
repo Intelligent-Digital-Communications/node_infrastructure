9a397bf47342b9deee904f40621dd5f1e72000b5
Flinner, Hayden <hayden@gatech.edu>
2015-11-21 10:43:58 -0500

    Update generate_epochs.py
    Fixed a bad commit.

--- generate_epochs.py
+++ generate_epochs.py
@@ -1,82 +1,104 @@
-import sys
-import re
 import os
-import glob
-GAINKEY = "--gain="
-PATHKEY = "--path="
-EXITCODE = -392
+import sys
+import datetime
 
-def print_help():
-    print("-----------update_gains.py-------------")
-    print("   - Looks for epoch*.sh files and changes the gain value within.")
-    print("   - Give a parameter of '--gain=##' to specify the new gain.")
-    print("   - Parameter '--path=/AAA/BBB/' to specify absolute path.")
-    print("---------------------------------------")
+def create_needed_directory(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return
 
-def process_parameters():
-    gain=EXITCODE
-    path=""
-    try:
-        for parameter in sys.argv:
-            if GAINKEY in parameter:
-                should_be_gain = parameter.split('=')[1]
-                if should_be_gain.isdigit():
-                    gain = int(should_be_gain)
-                else:
-                    return EXITCODE 
-            if PATHKEY in parameter:
-                should_be_path = parameter.split('=')[1]
-                if is_valid_path(should_be_path):
-                    path = should_be_path
-                else:
-                    return EXITCODE
-    except:
-        return EXITCODE
-    return (gain, path)
+def read_csv_from_parameters():
+    infile = open(sys.argv[1], 'r')
+    path = sys.argv[2]
 
-def is_valid_path(string):
-    return string.startswith('/') and string.endswith('/')
+    create_needed_directory(path)
+    # Try to fix the path incase user didn't put the /.
+    if not path.endswith("/"):
+        path = path + "/"
 
-def process_files(list_of_files, new_gain):
-    try:
-        print("-------------------------")
-        print("-----files processed-----")
-        print("-------------------------")
-        for filename in list_of_files:
-            print(filename)
-            in_file = open(filename, 'r')
-            launch_script = in_file.read()
+    path_for_log_file = path + "recordings"
+    create_needed_directory(path_for_log_file)
+    # Create the atqCmd file that will need to be ran later to schedule the
+    # records
+    atqCmd = open(path + "atCmd.sh", "w")
+    atqCmd.write("#!/bin/bash\n")
 
-            out_file = open(filename + ".tmp", 'w')
-            out_file.write(re.sub(r'--gain=\d+\b', "--gain="
-                + str(new_gain), launch_script))
+    successfully_written = 0
+    for line in infile:
+        # Mindless parsing...
+        items = line.split(',')
+        time = items[0]
+        filename_extension = items[1]
+        frequency = items[2]
+        length_of_epochs = items[3].strip() # Only necessary here beacuse this
+	    # this item contains a newline as it's the last one on the line
+        
+        # This is the filename for this specific shell script.
+        filename = "epoch" + filename_extension + ".sh"
+        #And this is how we know where to write it to
+        filename_with_path = path + filename
 
-            out_file.close()
-            in_file.close()
+        epoch_file = open(filename_with_path, 'w')
+        epoch_file.write("#!/bin/bash\n")
+        epoch_file.write('echo "' + filename + '" >> ' + path_for_log_file + '\n')
 
-            os.remove(filename)
-            os.rename(filename + ".tmp", filename)
-        print("--------------------------")
-        print("Files Edited: " + str(len(list_of_files)))
-        print("New Gain: " + str(new_gain))
-        print("--------------------------")
+        filename_for_specrec = path + "recordings/epoch" + filename_extension + ".sc16"
 
-    except IOError as e:
-        print(e)
-        print("Error during file reading/writing.")
+        write_string = 'specrec --args=master_clock_rate=25e6 --rate=25e6 --ant=RX2 --time=' + str(length_of_epochs) + ' --freq=' + frequency + ' --gain=50 --ref=gpsdo --metadata=true --segsize=24999936 --file=' + filename_for_specrec + ' --starttime="' + time + '" >> ' + path_for_log_file + ' 2>&1'
+        
+        epoch_file.write(write_string)
+        epoch_file.close()
+
+        print("Wrote epoch" + filename_extension + ".sh for starting time " + time)
+        
+        # Parse the specrec datetime into a datetime object
+        twoitems = time.split(' ')
+        dateinfo = twoitems[0].split('-')
+        timeinfo = twoitems[1].split(':')
+        datetime_object = datetime.datetime(int(dateinfo[0]), int(dateinfo[1]), int(dateinfo[2]), int(timeinfo[0]), int(timeinfo[1]), int(timeinfo[2]))
+        
+        # Schedule the epoch about a minute early with put_epoch_in_schedule()
+        put_epoch_in_schedule(filename_with_path, datetime_object, atqCmd)
+
+        successfully_written += 1
 
 def main():
-    gain, path = process_parameters()
-    if gain == EXITCODE:
-        print_help()
-        return
-    list_of_files = glob.glob(path + "epoch*.sh")
-    if len(list_of_files) == 0:
-        if path == "":
-            print("No epoch*.sh files found in current folder.")
-        else:
-            print("No epoch*.sh files found in " + path)
-        return
-    process_files(list_of_files, gain)
+    if len(sys.argv) > 1:
+        read_csv_from_parameters()
+    else:
+        # No args run
+        display_help()
+
+def display_help():
+    print("-------generate_epochs.py Information--------")
+    print("Takes a CSV file of start times, filename extensions,"
+    + " and frequencies.")
+    print("Give it a CSV file path and a path to write files to.")
+    print("Generates shell scripts for launching specrec according to CSV file")
+    print("")
+    print("Needed parameters: [CSV filename] [path to write to]")
+    print("Example run: python generate_epochs.py /opt/fall15alcornstate/schedule.csv /opt/fall15alcornstate/")
+    print("CSV Format: [specrec time string],[extension to epoch],[frequency],[timeForEpoch]")
+    print("For example: [2015-10-15 08:00:00,0,2406] would result in an epoch0.sh file")
+    print("that would tell specrec to start recording at 2015-10-15 08:00:00")
+    print("at frequency 2406.")
+    print("")
+    print("Default arguments passed to specrec:")
+    print("master_clock_rate = 25e6, rate=25e6, ant=RX2, gain=50, ref=gpsdo")
+    print("metadata=true, segsize=24999936")
+    print("")
+    print("Logs are written to [path to write to]/recordings/logs.txt")
+
+def fix_one_digit(string):
+    if len(string) == 1:
+        return "0" + string
+    return string
+
+def put_epoch_in_schedule(filename, timeobject, atqCmd):
+    timeobject = timeobject - datetime.timedelta(seconds=40)
+
+    atq_timedate_string = fix_one_digit(str(timeobject.hour)) + ":" + fix_one_digit(str(timeobject.minute)) + " " + fix_one_digit(str(timeobject.month)) + "/" + fix_one_digit(str(timeobject.day)) + "/" + str(timeobject.year)
+    
+    atqCmd.write('at ' + atq_timedate_string + ' -f ' + filename + '\n')
 
 main()
