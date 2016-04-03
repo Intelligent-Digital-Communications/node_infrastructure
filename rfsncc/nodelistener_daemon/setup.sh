852b76ffe1272d5b18f1c21e8aa840ec25574094
Hayden Flinner <hayden@gatech.edu>
2016-04-02 23:42:43 -0400

    Renamed folder.

---
+++ rfsncc/nodelistener_daemon/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+BINDIR=/usr/local/bin/NodeListener
+INITD=/etc/init.d
+PYSCRIPT=NodeListener.py
+SHSCRIPT=NodeListener.sh
+# Don't need to touch anything below here!
+PYPATH=$BINDIR/$PYSCRIPT
+SHPATH=$INITD/$SHSCRIPT
+mkdir $BINDIR
+cp $PYSCRIPT $PYPATH && chmod +x $PYPATH
+cp $SHSCRIPT $SHPATH && chmod +x $SHPATH
+$SHPATH start 
+$SHPATH status
+update-rc.d $SHSCRIPT defaults
+ls -l /etc/rc?.d/*$SHSCRIPT
