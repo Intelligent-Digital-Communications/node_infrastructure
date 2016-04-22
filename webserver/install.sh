ec69f63315c3a430c84bb439217a5b591be709fb
Hayden Flinner <hayden@gatech.edu>
2016-04-22 19:33:59 -0400

    Added django site.

---
+++ webserver/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#hostname
+#hostname -f
+#sudo apt-get install apache2 libapache2-mod-perl2
+
+
+
+
+# We put scripts in html/cgi/
+# Might need to go in /usr/lib/cgi-bin/
+
---
+++ webserver/simple.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl
+
+print "Content-Type: text/html \n\n";
+
+print "Welcome to the RFSN Control page!"
