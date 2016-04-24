c5d45dbb88210f39885e4a8d88d206e02d057994
Flinner, Hayden <hayden@gatech.edu>
2016-04-23 21:52:47 -0400

    Files are now written to /opt/CSVname

--- NodeListener/csv_scheduler.py
+++ NodeListener/csv_scheduler.py
@@ -6,6 +6,8 @@ import subprocess
 
 possibleENVpaths = ["/home/ops/sdr/setup_env.sh", "/home/ops/git-repos/target/setup_env.sh",
 			"/home/ops/git_repos/target/setup_env.sh" ]
+# TODO: Once NodeListener configured to not run as root, remove all traces
+# of ENV path hacks and remove the ". ENVPATH" line.
 
 def findENVfile():
     for envfile in possibleENVpaths:
@@ -14,7 +16,8 @@ def findENVfile():
     return "ERROR"
 
 def schedule_csv(infile):
-    path = os.getcwd() + '/recordings/'
+    # files are written to /opt/nameofCSV
+    path = infile.name.split('/')[-1].split('.')[0]
     path_for_log_file = path + 'log.txt'
     if not os.path.exists(path):
         os.makedirs(path)
@@ -30,8 +33,9 @@ def schedule_csv(infile):
 	length_of_epochs = items[3].strip()
 
 	filename_for_specrec = path + 'epoch' + filename_extension + ".sc16"
-	# Parse the specrec datetime into a datetime object
-	# TODO Replace with datetime POSIX standard, making code easier to read
+	# Parse the specrec datetime into a datetime object. Does Specrec use
+	# POSIX standard? If so, datetime probably supports parsing itself
+	# TODO: Offload date parsing to datetime module
 	twoitems = time.split(' ')
 	dateinfo = twoitems[0].split('-')
 	timeinfo = twoitems[1].split(':')
@@ -39,6 +43,8 @@ def schedule_csv(infile):
 	    int(dateinfo[2]), int(timeinfo[0]), int(timeinfo[1]),
 	    int(timeinfo[2]))
 	# Give datetime object as time to at
+	# TODO at supports POSIX standard, find a way to get a POSIX string
+	# from datetime object
 	datetime_object = datetime_object - datetime.timedelta(seconds=40)
 	atq_timedate_string = (
 		fix_one_digit(str(datetime_object.hour)) + ":"
@@ -46,7 +52,9 @@ def schedule_csv(infile):
 		+ fix_one_digit(str(datetime_object.month)) + "/"
 		+ fix_one_digit(str(datetime_object.day)) + "/"
 		+ str(datetime_object.year))
-	# TODO gain is hardcoded? Find alternative
+	# Gain is hardcoded here. Alternative?
+	# Frequency is hardcoded to include an "e6" as its what our hardware
+	# is optimized for. Remove if want to use different frequencies.
 	argslist = ['specrec',  '--args=master_clock_rate=25e6',
 		'--rate=25e6', '--ant=RX2', '--time=' + str(length_of_epochs),
 		'--freq=' + frequency + 'e6', '--gain=50', '--ref=gpsdo',
@@ -55,6 +63,8 @@ def schedule_csv(infile):
 		r'>>', path_for_log_file, r'2>&1' ]
 	filename = commandspath + 'epoch' + filename_extension + '.sh'
 	envfile = findENVfile()
+	# TODO The line below will probably break in Python 3, but if NodeListener
+	# doesn't need root then the if statement below can be removed entirely.
         if "ERROR" in envfile:
             keeptrack.append("ERROR: Unable to source Environment File."
 		+ " Specrec will fail unless you patch the epoch.sh files to "
@@ -65,6 +75,7 @@ def schedule_csv(infile):
 	epoch_file.close()
 	keeptrack.append(filename)
 	os.chmod(filename, os.stat(filename).st_mode | 0111) # Make exec by everyone
+	# TODO Set to only be executable by the current user for security
 	atqCmd.write('at ' + atq_timedate_string + ' -f ' + filename + '\n')
 	p = subprocess.Popen(
 	    ['at', atq_timedate_string, '-f', filename],
@@ -75,14 +86,13 @@ def schedule_csv(infile):
         if 'past' in err:
             keeptrack.append(' '.join(line.split(' ')[1:3]) + ' has already passed.')
             return keeptrack
-        # Probably launched the file wrong
 
         lines = err.split('\n')
         job_misc = lines[1].split(' at ') # 0th line is just a shell warning
 
         # At this point, job_misc[0] is 'job #' and job_misc[1] is 'Sat Nov...'
         job_id = job_misc[0].split(' ')[1] # Pull the job id
-        job_datetime =datetime.datetime.strptime(job_misc[1], "%c") # Create datetime object
+        job_datetime = datetime.datetime.strptime(job_misc[1], "%c") # Create datetime object
         keeptrack.append((argslist, job_id, job_datetime ))
     atqCmd.close()
     print("keeptrack:" + str(keeptrack))
@@ -93,25 +103,24 @@ def main():
         with open(sys.argv[1]) as csv:
             schedule_csv(csv)
     else:
-        # No args run
         help()
 
 def help():
     print("-------generate_epochs.py Information--------")
     print("Takes a CSV file of start times, filename extensions,"
     + " and frequencies.")
-    print("Give it a CSV file path and a path to write files to.")
     print("Generates shell scripts for launching specrec according to CSV file\n")
-    print("Needed parameters: [CSV filename] [path to write to]")
-    print("Example run: python generate_epochs.py /opt/fall15alcornstate/schedule.csv /opt/fall15alcornstate/")
-    print("CSV Format: [specrec time string],[extension to epoch],[frequency],[timeForEpoch]")
+    print("Also schedules the scripts in the "at" queue.")
+    print("Parameters: [CSV filename]")
+    print("Example run: python generate_epochs.py /~/csv_files/schedule.csv")
+    print("CSV Format: [specrec time string],[extension to epoch],[frequency],[epochLength]")
     print("For example: [2015-10-15 08:00:00,0,2406] would result in an epoch0.sh file")
     print("that would tell specrec to start recording at 2015-10-15 08:00:00")
     print("at frequency 2406.\n")
     print("Default arguments passed to specrec:")
     print("master_clock_rate = 25e6, rate=25e6, ant=RX2, gain=50, ref=gpsdo")
     print("metadata=true, segsize=24999936\n")
-    print("Logs are written to [path to write to]/recordings/logs.txt")
+    print("Specrec logs are written to [path to write to]/recordings/logs.txt")
 
 def fix_one_digit(string):
     if len(string) == 1:
