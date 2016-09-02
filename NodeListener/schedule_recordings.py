d44660028a8166e436413356ec1330201c82ca1c
Hayden Flinner <hayden@gatech.edu>
2016-09-02 18:38:03 -0400

    Latest progress on refactoring.

--- NodeListener/schedule_recordings.py
+++ NodeListener/schedule_recordings.py
@@ -7,85 +7,60 @@ import subprocess
 class Recording:
     """ Defines everything you need to know to schedule a record """
 
-    def __init__(self, timedate=None, recordpath=None, frequency=0,
-            length=0, startearly=40):
-        self.timedate = timedate
+    def __init__(self, starttime=None, recordpath=None, frequency=0,
+            length=0, startearly=40, logfilepath='log.txt', gain=50):
+        self.starttime = starttime
         self.recordpath = filename # ends in Sc16
         self.frequency = frequency
         self.length = length
         self.startearly = startearly
+        self.logfilepath = logfilepath
+        self.gain = gain
     
-def schedule_recordings(infile):
-    # files are written to /opt/nameofCSV
-    path = infile.name.split('/')[-1].split('.')[0]
-    path_for_log_file = path + 'log.txt'
-    if not os.path.exists(path):
-        os.makedirs(path)
-    keeptrack = []
+def schedule_recordings(recordingslist):
     commandspath = path + 'commands/'
     if not os.path.exists(commandspath):
         os.makedirs(commandspath)
     atqCmd = open(commandspath + 'atqCmd.sh', 'w')
     atqCmd.write('#!/bin/bash\n')
-    for line in infile:
-	items = line.split(',')
-	time, filename_extension, frequency= items[0], items[1], items[2]
-	length_of_epochs = items[3].strip()
+    for recording in recordingslist:
+        path_for_log_file = recording.logfilepath
+        recordfolder = recording.recordpath.split('/')[-1].split('.')[0]
+        if not os.path.exists(recordfolder):
+            os.makedirs(recordfolder)
+        time, filename_extension, frequency= items[0], items[1], items[2]
+        length_of_epochs = items[3].strip()
+        filename_for_specrec = recording.recordpath
+        datetime_object = recording.start - datetime.timedelta(
+                seconds=recording.startearly)
 
-	filename_for_specrec = path + 'epoch' + filename_extension + ".sc16"
-	# Parse the specrec datetime into a datetime object. Does Specrec use
-	# POSIX standard? If so, datetime probably supports parsing itself
-	# TODO: Offload date parsing to datetime module
-	twoitems = time.split(' ')
-	dateinfo = twoitems[0].split('-')
-	timeinfo = twoitems[1].split(':')
-	datetime_object = datetime.datetime(int(dateinfo[0]), int(dateinfo[1]),
-	    int(dateinfo[2]), int(timeinfo[0]), int(timeinfo[1]),
-	    int(timeinfo[2]))
-	# Give datetime object as time to at
-	# TODO at supports POSIX standard, find a way to get a POSIX string
-	# from datetime object
-	datetime_object = datetime_object - datetime.timedelta(seconds=40)
-	atq_timedate_string = (
-		fix_one_digit(str(datetime_object.hour)) + ":"
-		+ fix_one_digit(str(datetime_object.minute)) + " "
-		+ fix_one_digit(str(datetime_object.month)) + "/"
-		+ fix_one_digit(str(datetime_object.day)) + "/"
-		+ str(datetime_object.year))
-	# Gain is hardcoded here. Alternative?
-	# Frequency is hardcoded to include an "e6" as its what our hardware
-	# is optimized for. Remove if want to use different frequencies.
-	argslist = ['specrec',  '--args=master_clock_rate=25e6',
-		'--rate=25e6', '--ant=RX2', '--time=' + str(length_of_epochs),
-		'--freq=' + frequency + 'e6', '--gain=50', '--ref=gpsdo',
-		'--metadata=true', '--segsize=24999936',
-		'--file=' + filename_for_specrec, r'--starttime="' + time + r'"',
-		r'>>', path_for_log_file, r'2>&1' ]
-	filename = commandspath + 'epoch' + filename_extension + '.sh'
-	envfile = findENVfile()
-	# TODO The line below will probably break in Python 3, but if NodeListener
-	# doesn't need root then the if statement below can be removed entirely.
-        if "ERROR" in envfile:
-            keeptrack.append("ERROR: Unable to source Environment File."
-		+ " Specrec will fail unless you patch the epoch.sh files to "
-		+ "source the path where specrec is installed.")
-	epoch_file = open(filename, 'w')
-	epoch_file.write('#!/bin/bash\n. {}\necho {} >> {}\n{}'
-			.format(envfile, filename, path_for_log_file, ' '.join(argslist)))
-	epoch_file.close()
-	keeptrack.append(filename)
-	os.chmod(filename, os.stat(filename).st_mode | 0111) # Make exec by everyone
-	# TODO Set to only be executable by the current user for security
-	atqCmd.write('at ' + atq_timedate_string + ' -f ' + filename + '\n')
-	p = subprocess.Popen(
-	    ['at', atq_timedate_string, '-f', filename],
-	    stdout=subprocess.PIPE,
-	    stderr=subprocess.PIPE,
-	    )
-	output, err = p.communicate()
+        atq_timedate_string = datetime_object.strftime('%H:%M %m/%d/%Y')
+
+        args = 'specrec --args=master_clock_rate=25e6 --rate=25e6 --ant=RX2 \
+                --time={length} --freq={frequency} --gain={gain} --ref=gpsdo \
+                --metadata=true --segsize=24999936 --file={specrecfilename} \
+                --starttime="{start}" >> {logfilepath} 2>&1'.format(
+                length=recording.length, freq=recording.frequency,
+                gain=recording.gain, specrecfilename=recording.recordpath,
+                start=recording.starttime.isoformat(' '),
+                logfilepath = recording.logfilepath)
+        filename = commandspath + 'epoch' + filename_extension + '.sh'
+        epoch_file = open(filename, 'w')
+        epoch_file.write('#!/bin/bash\n. {}\necho {} >> {}\n{}'
+                        .format(envfile, filename, path_for_log_file, ' '.join(argslist)))
+        epoch_file.close()
+        os.chmod(filename, os.stat(filename).st_mode | int("0111", 8)) # Make exec by everyone
+        # TODO Set to only be executable by the current user for security
+        atqCmd.write('at ' + atq_timedate_string + ' -f ' + filename + '\n')
+        p = subprocess.Popen(
+            ['at', atq_timedate_string, '-f', filename],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            )
+        output, err = p.communicate()
         if 'past' in err:
-            keeptrack.append(' '.join(line.split(' ')[1:3]) + ' has already passed.')
-            return keeptrack
+            pass
+            # The one you just scheduled is already over!
 
         lines = err.split('\n')
         job_misc = lines[1].split(' at ') # 0th line is just a shell warning
@@ -93,10 +68,7 @@ def schedule_recordings(infile):
         # At this point, job_misc[0] is 'job #' and job_misc[1] is 'Sat Nov...'
         job_id = job_misc[0].split(' ')[1] # Pull the job id
         job_datetime = datetime.datetime.strptime(job_misc[1], "%c") # Create datetime object
-        keeptrack.append((argslist, job_id, job_datetime ))
     atqCmd.close()
-    print("keeptrack:" + str(keeptrack))
-    return keeptrack
 
 def main():
     if len(sys.argv) > 1:
