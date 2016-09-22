af87af257bdb714786c306e207f2ed63b8a309e2
Ryan Gallaway <rgallaway@gatech.edu>
2016-09-21 21:08:05 -0400

    added Orin to email list

--- django/myproject/myapp/views.py
+++ django/myproject/myapp/views.py
@@ -50,7 +50,7 @@ def schedule_recordings(request, hostname):
             'If you\'re seeing this, scheduling was probably successful!' +
             '\n\n' + result.text,
             'idc.gatech@gmail.com',
-            ['rgallaway@gatech.edu', 'haydenflinner@gmail.com'],
+            ['rgallaway@gatech.edu', 'haydenflinner@gmail.com', 'orindlincoln@gatech.edu],
             fail_silently=False
         )                                                          
         return HttpResponse(result)
