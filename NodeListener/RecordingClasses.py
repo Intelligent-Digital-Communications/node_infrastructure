aae8ce031f9119ee13d2d09e300c949a31d3dbbf
Patel, Kavish M <kpatel6987@gatech.edu>
2016-09-29 19:29:33 -0400

    Created new Session class; Moved recording class

---
+++ NodeListener/RecordingClasses.py
@@ -0,0 +1,24 @@
+import os, shutil, stat, sys, datetime, subprocess
+
+class Recording:
+    """ Defines everything you need to know to schedule a record """
+
+    def __init__(self, starttime=None, recordpath=None, frequency=0,
+            length=0, startearly=40, logfilepath='log.txt', gain=50,
+            include='include/'):
+        self.starttime  = datetime.datetime.strptime(starttime, "%m/%d/%Y %H:%M")
+        self.recordpath = recordpath # ends in Sc16
+        self.frequency = float(frequency)
+        self.length = int(length)
+        self.startearly = int(startearly)
+        self.logfilepath = logfilepath
+        self.gain = int(gain)
+        self.include = include
+
+class Session:
+
+    def __init__(self, logpath = 'log.txt', startingpath=None, hostname=None):
+        self.logpath = logpath
+        self.startingpath = startingpath
+        self.hostname = hostname
+        
