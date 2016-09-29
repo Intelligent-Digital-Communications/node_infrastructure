5c8d418ffd78f9eb39fd405561ac5d29418a32d8
Patel, Kavish M <kpatel6987@gatech.edu>
2016-09-29 19:27:37 -0400

    Update schedule_recordings.py

--- NodeListener/schedule_recordings.py
+++ NodeListener/schedule_recordings.py
@@ -1,19 +1,5 @@
 import os, shutil, stat, sys, datetime, subprocess
 
-class Recording:
-    """ Defines everything you need to know to schedule a record """
-
-    def __init__(self, starttime=None, recordpath=None, frequency=0,
-            length=0, startearly=40, logfilepath='log.txt', gain=50,
-            include='include/'):
-        self.starttime  = datetime.datetime.strptime(starttime, "%m/%d/%Y %H:%M")
-        self.recordpath = recordpath # ends in Sc16
-        self.frequency = float(frequency)
-        self.length = int(length)
-        self.startearly = int(startearly)
-        self.logfilepath = logfilepath
-        self.gain = int(gain)
-        self.include = include
 
 def getfolder(path):
     return '/'.join(path.split('/')[:-1])
