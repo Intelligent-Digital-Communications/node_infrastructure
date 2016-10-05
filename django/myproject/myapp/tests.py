736fcd65c79f6a87e72b9115411df382aa3573f8
Hayden Flinner <hayden@gatech.edu>
2016-10-04 21:28:37 -0400

    Added proper test format.

--- django/myproject/myapp/tests.py
+++ django/myproject/myapp/tests.py
@@ -3,8 +3,13 @@ from django.test import Client
 from .csvtojson import convert
 
 # Example test for posting a CSV, need to create a jsonSessionDict
-c = Client()
-with open('myproject/myapp/csv/test0.csv', 'rb') as csv:
-    response = c.post('/myapp/upload_file/', { 'docfile' : csv, 'rfsns' : 1 })
-    print(response.status_code)
-    print(response.content)
+class ScheduleAndCancelTestCase(TestCase):
+    def test_schedule_then_cancel(self):
+        c = Client()
+        with open('myproject/myapp/csv/xmas2017.csv', 'rb') as csv:
+            response = c.post('/myapp/upload_file/', { 'docfile' : csv, 'rfsns' : [1,2,3] })
+            self.assertTrue(response.status_code == 200)
+        # Commented out because endpoint doesn't exist in controller yet 
+        #response = c.get('/myapp/clear_atq/', { 'rfsns' : 1,2,3 })
+        #self.assertTrue(response.status_code == 200)
+    
