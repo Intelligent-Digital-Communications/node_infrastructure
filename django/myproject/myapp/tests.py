2f634dfa0444c4809041fb29dfad16a79c2c9fe3
Patel, Kavish M <kpatel6987@gatech.edu>
2016-10-08 21:36:00 -0400

    Changed docfile to file

--- django/myproject/myapp/tests.py
+++ django/myproject/myapp/tests.py
@@ -7,7 +7,7 @@ class ScheduleAndCancelTestCase(TestCase):
     def test_schedule_then_cancel(self):
         c = Client()
         with open('myproject/myapp/csv/xmas2017.csv', 'rb') as csv:
-            response = c.post('/myapp/upload_file/', { 'docfile' : csv, 'rfsns' : [1,2,3] })
+            response = c.post('/myapp/upload_file/', { 'file' : csv, 'rfsns' : [1,2,3] })
             self.assertTrue(response.status_code == 200)
         # Commented out because endpoint doesn't exist in controller yet 
         #response = c.get('/myapp/clear_atq/', { 'rfsns' : 1,2,3 })
