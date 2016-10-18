3d4c744d7566d0790e9402df71648ccc257dd183
Hayden Flinner <hayden@gatech.edu>
2016-10-17 19:32:57 -0700

    Added uniques property.

--- NodeListener/RecordingClasses.py
+++ NodeListener/RecordingClasses.py
@@ -3,7 +3,8 @@ import os, shutil, stat, sys, datetime, subprocess
 class Recording:
     """ Defines everything you need to know to schedule a record """
 
-    def __init__(self, starttime, recordpath, frequency, length, startearly=40, gain=50, jobid=None):
+    def __init__(self, starttime, recordpath, frequency, length, startearly=40,
+            gain=50, jobid=None, uniques=None):
         self.starttime  = datetime.datetime.strptime(starttime, "%m/%d/%Y %H:%M")
         self.recordpath = recordpath # ends in Sc16
         self.frequency = float(frequency)
@@ -12,6 +13,8 @@ class Recording:
         self.gain = int(gain)
         if jobid:
             self.jobid = int(jobid)
+        if uniques:
+            self.uniques = uniques
 
 class Session:
 
