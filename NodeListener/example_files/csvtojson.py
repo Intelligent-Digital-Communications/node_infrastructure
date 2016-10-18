830530887933d983cfb6885c5f8114f1bcc5e31b
Hayden Flinner <hayden@gatech.edu>
2016-10-17 17:39:02 -0700

    Converted CSVtoJSON to make Sessions.

--- NodeListener/example_files/csvtojson.py
+++ NodeListener/example_files/csvtojson.py
@@ -3,18 +3,22 @@ import csv, json, sys
 
 def convert(csvname):
     with open(csvname, 'r') as csvfile:
-        fieldnames = ("starttime","recordpath","frequency","length","startearly",
-                "logfilepath","gain")
+        fieldnames = ["starttime","recordpath","frequency","length","startearly",
+                "logfilepath","gain"]
         recordings = []
-        master_dict = { 'recordings' : recordings }
-        reader = csv.DictReader(csvfile, fieldnames)
+        reader = csv.reader(csvfile, fieldnames)
+        vals = next(reader)
         for row in reader:
-            if not row['starttime'].startswith('#'): # Assumes startime is 1st
+            if not row[0].startswith('#'): # Assumes startime is 1st
                 recordings.append(row)
+        master_dict = { 'recordings' : recordings,
+                'startingpath'  : vals[0],
+                'logpath'       : vals[1],
+                'rfsnid'        : vals[2] }
     return json.dumps(master_dict)
 
 if __name__ ==  "__main__":
     if len(sys.argv) > 1:
         print(convert(sys.argv[1]))
     else:
-	print("Give csv name of schedule as parameter.")
+        print("Give csv name of schedule as parameter.")
