2068d119ff40c46bb82dba80fa65ac8c81dd16fc
Hayden Flinner <hayden@gatech.edu>
2016-10-28 16:03:18 -0400

    another change lmao

--- django/myproject/myapp/NodeListener/NodeListener.py
+++ django/myproject/myapp/NodeListener/NodeListener.py
@@ -59,7 +59,7 @@ def filedrop(body):
 
         dpath = 'uploader@idc2.vip.gatech.edu:' + dpath + '/' + date + '_' + game + '/' + 'rfsn' + rfsnid + '/' + 'pred/'
         print(dpath)
-        atargs = ['rsync', '-av', spath, dpath, '']
+        atargs = ['mkdir', dpath, 'rsync', '-av', spath, dpath, '']
         Popen(atargs, stdout=PIPE, stderr=PIPE)
         return 'success'
     except Exception as e:
