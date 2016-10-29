99003afc0ea2aa5ad3cde04493d7fee95c0ad4d9
Hayden Flinner <hayden@gatech.edu>
2016-10-29 18:13:45 -0400

    change

--- django/myproject/myapp/NodeListener/NodeListener.py
+++ django/myproject/myapp/NodeListener/NodeListener.py
@@ -59,11 +59,12 @@ def filedrop(body):
         commonpath = 'uploader@idc2.vip.gatech.edu:/home/idcjbod/filedrop'
 
         folderpath = fpath + '/' + date + '_' + game + '/' + 'rfsn' + rfsnid + '/' + 'pred/'        #/test/20161029_duke/rfsn1/pred/
-        dpath = commonpath + fpath      #uploader@idc2.vip.gatech.edu:/home/idcjbod/filedrop/test/20161029_duke/rfsn1/pred/
+        dpath = commonpath + '/' + fpath      #uploader@idc2.vip.gatech.edu:/home/idcjbod/filedrop/test/20161029_duke/rfsn1/pred/
 
 
         atargs = ['mkdir', '-p', folderpath]#, '&&', 'rsync', '-av', spath, dpath, '']
         Popen(atargs, stdout=PIPE, stderr=PIPE)
+        print(atargs)
         atargs = ['rsync', '-av', fpath, commonpath]
         Popen(atargs, stdout=PIPE, stderr=PIPE)
         atargs = ['rsync', '-av', spath, folderpath]
