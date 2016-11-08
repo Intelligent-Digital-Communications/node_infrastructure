2eacb2cb2865af9f2ef96e43ca66caf727501f75
Hayden Flinner <hayden@gatech.edu>
2016-11-07 21:13:31 -0500

    anotha change

--- django/myproject/myapp/tests.py
+++ django/myproject/myapp/tests.py
@@ -3,9 +3,11 @@ from django.test import Client
 from django.core import mail
 from .csvtojson import convert
 from .NodeListener import *
+#from NodeListener import filedrop
 import os
 
 # Example test for posting a CSV, need to create a jsonSessionDict
+'''
 class ScheduleAndCancelTestCase(TestCase):
     def test_schedule_then_cancel(self):
         c = Client()
@@ -15,7 +17,11 @@ class ScheduleAndCancelTestCase(TestCase):
             self.assertEqual(len(mail.outbox), 1)
             s = Util.loads(response.content.decode('utf-8'))
             print(s)
-        # Commented out because endpoint doesn't exist in controller yet 
-        #response = c.get('/myapp/clear_atq/', { 'rfsns' : 1,2,3 })
-        #self.assertTrue(response.status_code == 200)
-    
+'''
+class TestFiledropSession(TestCase):
+    def runTest(self):
+        c = Client()
+        passing = {'spath': '/home/ops/testfolder/', 'rfsnid':'1', 'fpath':'test', 'date':'20161029', 'game':'duke', 'scheduletime': '2:30 PM 11/15/2016'}
+        response = c.post('/myapp/filedrop/1/', passing)
+        print(response)
+        #print(filedrop(json.dumps(passing)))
