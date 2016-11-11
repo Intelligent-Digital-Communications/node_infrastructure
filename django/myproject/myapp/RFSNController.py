934edfd5a21dbc876a4cf2868639c7594f5a03a9
Hayden Flinner <hayden@gatech.edu>
2016-11-10 23:25:15 -0500

    change

--- django/myproject/myapp/RFSNController.py
+++ django/myproject/myapp/RFSNController.py
@@ -23,27 +23,21 @@ def schedule(session, rfsn):
     url = "http://" + listeners[int(rfsn)] + "/generate_epochs/";
     print("SCHEDULE URL: " + url)
     req = requests.post(url, data=Util.dumps(session))
-    formattedDate = session.recordings[-1].starttime.strftime("%d%m%Y");
-    formattedScheduleTime = (session.recordings[-1].starttime.replace(hour=0) + datetime.timedelta(days=1)).strftime("%H:%M %m/%d/%Y");
-    data = {'spath': session.startingpath, 'rfsnid': rfsn, 'fpath':'test', 'date': formattedDate, 'game':'gatech', 'scheduletime': formattedScheduleTime }
-    file_drop(data, rfsn);
+    file_drop(session, rfsn);
     return req
 
-#def genericfunction(jsondata, functionname, rfsn):
-#    url = "http://" + listeners[int(rfsn)] + functionname;
-#    print("SCHEDULE URL: " + url)
-#    req = requests.post(url, json=jsondata)
-#    print(jsondata)
-#    return req
-
-def file_drop(data, hostname):
-    print(data['rfsnid'])
-    rfsn = data['rfsnid']
+def file_drop(session, rfsn):
+    formattedDate = session.recordings[-1].starttime.strftime("%d%m%Y");
+    formattedScheduleTime = (session.recordings[-1].starttime.replace(hour=(rfsn*2))
+        + datetime.timedelta(days=1)).strftime("%H:%M %m/%d/%Y");
+    data = {'spath': session.startingpath, 'rfsnid': rfsn, 'fpath':'test',
+        'date': formattedDate, 'game':'gatech',
+        'scheduletime': formattedScheduleTime, }
     jsonData = Util.dumps(data)
     url = "http://" + listeners[int(rfsn)] + "/filedrop/";
     #url = "http://" + "rfsn-demo1.vip.gatech.edu:8000"	+ "/filedrop/";
     print("CopyPaste URL: " + url)
-    req = requests.post(url, data=json.dumps(data))
+    req = requests.post(url, data=jsonData)
     return req
 
 def getatq():
