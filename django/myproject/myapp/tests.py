31e1be75aa88518f5c58941feaf5dfdf4da7e7df
Yeshy Manoharan <yeshymanoharan@gmail.com>
2017-02-17 15:03:14 -0500

    test for scheduling soon

--- django/myproject/myapp/tests.py
+++ django/myproject/myapp/tests.py
@@ -1,13 +1,14 @@
 from django.test import TestCase
 from django.test import Client
 from django.core import mail
+from datetime import datetime, timedelta
 from .csvtojson import convert
 from .NodeListener import *
 #from NodeListener import filedrop
 import os
 
 # Example test for posting a CSV, need to create a jsonSessionDict
-
+'''
 class ScheduleAndCancelTestCase(TestCase):
     def test_schedule_then_cancel(self):
         c = Client()
@@ -17,6 +18,18 @@ class ScheduleAndCancelTestCase(TestCase):
             self.assertEqual(len(mail.outbox), 1)
             s = Util.loads(response.content.decode('utf-8'))
             print(s)
+'''
+
+class ScheduleSoonAndCancelTestCase(TestCase):
+    def test_schedule_soon_then_cancel(self):
+        with open('myproject/myapp/csv/controller_test_schedule.csv', 'wb') as csv:
+            now = datetime.datetime.now()
+            now_plus_one = now + timedelta(minute=10)
+            csvwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            cvswriter.writerow(['TestGame1','/opt/spring17/','spring17_test.log','1','2','3'])
+            for i in range(1,10):
+                cvswriter.writerow([(now + timedelta(minute=1*i)).strftime('%DD/%MM/%YYYY %H:%M:%S'), 'epoch_test' + str(i) + '.sc16', '2.41E+09','5','60','55'])
+
 '''
 class TestFiledropSession(TestCase):
     def runTest(self):
