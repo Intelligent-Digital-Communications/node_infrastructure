d8a58a2b307ced521018fdd98dbdb89e2d8f6e09
Yeshy Manoharan <yeshymanoharan@gmail.com>
2017-02-27 17:54:23 -0500

    change to tests

--- django/myproject/myapp/tests.py
+++ django/myproject/myapp/tests.py
@@ -31,10 +31,10 @@ class ScheduleSoonAndCancelTestCase(TestCase):
             now = datetime.datetime.now()
             csvwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
             #change 1,2,3 to 0 for local testing
-            #csvwriter.writerow(['TestGame1', '/tmp/' + now.strftime('%H-%M') + '/',
-                #'spring17_test.log', '1', '2', '3'])
+            # csvwriter.writerow(['TestGame1', '/tmp/' + now.strftime('%H-%M') + '/',
+            #    'spring17_test.log', '60', '1', '2', '3'])
             csvwriter.writerow(['TestGame1', '/tmp/' + now.strftime('%H-%M') + '/',
-                'spring17_test.log', '60', '0'])
+                'spring17_test.log', '60', '1', '2', '3'])
             for i in range(1,10):
                 csvwriter.writerow([(now + timedelta(minutes=1*i)).strftime('%m/%d/%Y %H:%M'), 
                 'epoch_test' + str(i) + '.sc16', '2.41E+09', '3', '55'])
