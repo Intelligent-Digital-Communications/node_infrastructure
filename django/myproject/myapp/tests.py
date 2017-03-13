88275ddc14f96d3b3d8b59b19ac03d822586e45a
Yeshy Manoharan <yeshymanoharan@gmail.com>
2017-03-13 18:20:32 -0400

    eyo test

--- django/myproject/myapp/tests.py
+++ django/myproject/myapp/tests.py
@@ -40,9 +40,9 @@ class ScheduleSoonAndCancelTestCase(TestCase):
             csvwriter.writerow(['TestGame1', '/tmp/' + now.strftime('%H-%M') + '/',
                 'spring17_test.log', '60', '25e6', '1', '2', '3'])
             for i in range(1,10):
-                csvwriter.writerow([(now + timedelta(minutes=1*i)).strftime('%m/%d/%Y %H:%M'),
-                'epoch_test' + str(i) + '.sc16', '2.41E+09', '3', '60', '55'])
-
+                csvwriter.writerow([(now + timedelta(minutes=1*i)).strftime('%m/%d/%Y %H:%M'), 
+                'epoch_test' + str(i) + '.sc16', '2.41E+09', '3', '55'])
+        
         c = Client()
         with open('myproject/myapp/csv/controller_test_schedule.csv', 'rb') as csvfile:
             response = c.post('/myapp/upload_file/', { 'docfile' : csvfile, 'rfsns' : [1] })
@@ -53,17 +53,6 @@ class ScheduleSoonAndCancelTestCase(TestCase):
 
     def tearDown(self):
         print(RecordingModel.objects.all())
-
-class ListRFSNsTest(TestCase):
-    def setUp(self):
-        RFSN.objects.create(name="localhost", hostname="localhost", port=8000, pk=0)
-        RFSN.objects.create(name="testRFSN", hostname="yourmom", port=8080, pk=1)
-
-    def test_listRFSNs(self):
-        c = Client()
-        response = c.get("/myapp/list/")
-        print(response.content)
-
 
 
 '''
@@ -77,19 +66,8 @@ class TestFiledropSession(TestCase):
 '''
 
 class GetATQTestCase(TestCase):
-    def setUp(self):
-        RFSN.objects.create(name="localhost", hostname="localhost", port=8000, pk=0)
-        RFSN.objects.create(name="rfsn1", hostname="rfsn1", port=5035, pk=1)
-        RFSN.objects.create(name="rfsn2", hostname="rfsn2", port=5035, pk=2)
-        RFSN.objects.create(name="rfsn3", hostname="rfsn3", port=5035, pk=3)
-
     def test_get_atq(self):
-        c = Client()
-        for i in range(1,2):
-            print(str(i))
-            response = c.post('/myapp/getatq/' + str(i))
+        for i in range[1,3]:
+            response = c.post('/myapp/getatq/' + str(i), { 'docfile' : csvfile, 'rfsns' : [1] })
             s = Util.loads(response.content.decode('utf-8'))
             print(s)
-
-    def tearDown(self):
-        print(RecordingModel.objects.all())
